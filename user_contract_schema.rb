fa9549059dcdfffcc43c5da0c9601c0ae35201ac
Calin <calinoiu.alexandru@agilefreaks.com>
2016-11-21 10:56:17 +0530

    Age is optional

--- user_contract_schema.rb
+++ user_contract_schema.rb
@@ -3,13 +3,21 @@ require 'dry-validation'
 input_data = {
   name: 'John Doe',
   phone_number: 12345,
-  age: 17
+  age: 17,
+  address: {
+    street: 'Viapar Kendra',
+    city: nil
+  }
 }
 
 UserContractSchema = Dry::Validation.Schema do
   required(:name).filled
   required(:phone_number).filled
   required(:age) { none? | (filled? & int? & gt?(18)) }
+  required(:address).schema do
+    required(:street) { filled? }
+    optional(:city)
+  end
 end
 
 result = UserContractSchema.call(input_data)
