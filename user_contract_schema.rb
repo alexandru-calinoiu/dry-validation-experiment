07e4f482aad95181aac300c1443ec896a0252935
Calin <calinoiu.alexandru@agilefreaks.com>
2016-11-21 10:51:04 +0530

    Age validation

--- user_contract_schema.rb
+++ user_contract_schema.rb
@@ -2,12 +2,14 @@ require 'dry-validation'
 
 input_data = {
   name: 'John Doe',
-  phone_number: 12345
+  phone_number: 12345,
+  age: 17
 }
 
 UserContractSchema = Dry::Validation.Schema do
   required(:name).filled
   required(:phone_number).filled
+  required(:age) { filled? & int? & gt?(18) }
 end
 
 result = UserContractSchema.call(input_data)
